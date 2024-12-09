650fb02eaa0b75ac929a7b760742f29c5060924e
Odeth Ayellén Peña Gómez <odethayellen@hotmail.com>
2024-12-09 00:08:59 -0600

    btn Average, btn Add, calcular promedio guardando los datos en linked list

--- Troubleshooting_LinearStructures/Form1.Designer.cs
+++ Troubleshooting_LinearStructures/Form1.Designer.cs
@@ -29,11 +29,18 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            label1 = new Label();
-            txtWord = new TextBox();
-            lblWord = new Label();
             btnReverse = new Button();
+            lblWord = new Label();
+            txtWord = new TextBox();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            btnAverage = new Button();
+            lblAverage = new Label();
+            txtQualification = new TextBox();
+            label3 = new Label();
+            btnAdd = new Button();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
@@ -48,31 +55,6 @@
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(36, 48);
-            label1.Name = "label1";
-            label1.Size = new Size(36, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Word";
-            // 
-            // txtWord
-            // 
-            txtWord.Location = new Point(78, 45);
-            txtWord.Name = "txtWord";
-            txtWord.Size = new Size(100, 23);
-            txtWord.TabIndex = 1;
-            // 
-            // lblWord
-            // 
-            lblWord.AutoSize = true;
-            lblWord.Location = new Point(36, 121);
-            lblWord.Name = "lblWord";
-            lblWord.Size = new Size(36, 15);
-            lblWord.TabIndex = 2;
-            lblWord.Text = "Word";
-            // 
             // btnReverse
             // 
             btnReverse.Location = new Point(103, 85);
@@ -83,16 +65,103 @@
             btnReverse.UseVisualStyleBackColor = true;
             btnReverse.Click += btnReverse_Click;
             // 
+            // lblWord
+            // 
+            lblWord.AutoSize = true;
+            lblWord.Location = new Point(36, 121);
+            lblWord.Name = "lblWord";
+            lblWord.Size = new Size(36, 15);
+            lblWord.TabIndex = 2;
+            lblWord.Text = "Word";
+            // 
+            // txtWord
+            // 
+            txtWord.Location = new Point(78, 45);
+            txtWord.Name = "txtWord";
+            txtWord.Size = new Size(100, 23);
+            txtWord.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Word";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnAdd);
+            groupBox2.Controls.Add(btnAverage);
+            groupBox2.Controls.Add(lblAverage);
+            groupBox2.Controls.Add(txtQualification);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Location = new Point(383, 71);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(271, 166);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            // 
+            // btnAverage
+            // 
+            btnAverage.Location = new Point(137, 117);
+            btnAverage.Name = "btnAverage";
+            btnAverage.Size = new Size(75, 23);
+            btnAverage.TabIndex = 1;
+            btnAverage.Text = "Average";
+            btnAverage.UseVisualStyleBackColor = true;
+            btnAverage.Click += btnAverage_Click;
+            // 
+            // lblAverage
+            // 
+            lblAverage.AutoSize = true;
+            lblAverage.Location = new Point(36, 121);
+            lblAverage.Name = "lblAverage";
+            lblAverage.Size = new Size(16, 15);
+            lblAverage.TabIndex = 2;
+            lblAverage.Text = "...";
+            lblAverage.Click += lblAverage_Click;
+            // 
+            // txtQualification
+            // 
+            txtQualification.Location = new Point(123, 45);
+            txtQualification.Name = "txtQualification";
+            txtQualification.Size = new Size(100, 23);
+            txtQualification.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(36, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Qualification: ";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(137, 85);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 3;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(631, 293);
+            ClientSize = new Size(705, 293);
+            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -103,5 +172,11 @@
         private TextBox txtWord;
         private Label label1;
         private Button btnReverse;
+        private GroupBox groupBox2;
+        private Button btnAverage;
+        private Label lblAverage;
+        private TextBox txtQualification;
+        private Label label3;
+        private Button btnAdd;
     }
 }
--- Troubleshooting_LinearStructures/Form1.cs
+++ Troubleshooting_LinearStructures/Form1.cs
@@ -1,10 +1,13 @@
 namespace Troubleshooting_LinearStructures
 {
     public partial class Form1 : Form
+
     {
+        List<int> qualifications;
         public Form1()
         {
             InitializeComponent();
+            qualifications = new List<int>();
         }
 
         private void btnReverse_Click(object sender, EventArgs e)
@@ -30,7 +33,46 @@ namespace Troubleshooting_LinearStructures
                 reversedWord += stack.Pop();
             }
 
-            lblWord.Text= ("The reversed word is: " + reversedWord);
+            lblWord.Text = ("The reversed word is: " + reversedWord);
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            if (qualifications.Count > 0)
+            {
+                // Calcular el promedio usando LINQ
+                double promedio = qualifications.Average();
+                // Mostrar el promedio en el Label
+                lblAverage.Text = "Promedio: " + promedio.ToString("F2");
+            }
+            else
+            {
+                // Si no hay calificaciones, mostrar un mensaje
+                MessageBox.Show("No hay calificaciones para calcular el promedio.");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtQualification.Text, out int calificacion))
+            {
+                // Agregar la calificación a la lista
+                qualifications.Add(calificacion);
+                // Limpiar el TextBox para ingresar una nueva calificación
+                txtQualification.Clear();
+                // Mostrar un mensaje de éxito
+                MessageBox.Show("Calificación agregada correctamente.");
+            }
+            else
+            {
+                // Si no es un número válido, mostrar un mensaje de error
+                MessageBox.Show("Por favor ingresa una calificación válida.");
+            }
+        }
+
+        private void lblAverage_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
